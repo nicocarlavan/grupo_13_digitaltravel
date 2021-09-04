56a0b816b6ba541f3006385e62cdffe66093c2fc
nicocarlavan <nfernandez22@gmail.com>
2021-09-04 14:22:22 -0400

    Validacion completa con Express-Validator

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const bcrypt = require('bcryptjs');
-
+const { validationResult } = require('express-validator');
 const usersFilePath = path.join(__dirname, '../data/users.json');
 let users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
@@ -12,25 +12,28 @@ const usersController = {
 
     },
     registro: (req, res) => {
-        res.render('./users/register')
+        res.render('./users/register');
 
     },
     newUser: (req, res) => {
-        if (req.body.password == req.body.repassword) {
-            let newUser = {
-                id: users[users.length - 1].id + 1,
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                email: req.body.email,
-                password: bcrypt.hashSync(req.body.password, 10),
-                image: req.file.filename,
-            };
-            users.push(newUser);
-            fs.writeFileSync(usersFilePath, JSON.stringify(users));
-            res.redirect('/');
-        } else {
-            res.redirect('/register');
+        const resultValidation = validationResult(req);
+        if (resultValidation.errors.length > 0) {
+            res.render('./users/register', {
+                errors: resultValidation.mapped(),
+                oldData: req.body
+            });
         }
+        let newUser = {
+            id: users[users.length - 1].id + 1,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, 10),
+            image: req.file.filename,
+        };
+        users.push(newUser);
+        fs.writeFileSync(usersFilePath, JSON.stringify(users));
+        res.redirect('/');
 
     }
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const path = require('path');
-
+const { body } = require('express-validator');
 const multer = require('multer');
 
 var storage = multer.diskStorage({
@@ -17,10 +17,34 @@ let upload = multer({ storage: storage });
 
 const usersController = require('../controllers/usersController')
 
+const validations = [
+    body('firstName').notEmpty().withMessage('Debes completar el campo Nombre'),
+    body('lastName').notEmpty().withMessage('Debes completar el campo Apellido'),
+    body('email')
+        .notEmpty().withMessage('Debes completar el campo E-mail').bail()
+        .isEmail().withMessage('Debes escribir un formato de e-mail válido'),
+    body('password').notEmpty().withMessage('Debes completar el campo Contraseña'),
+    body('repassword')
+        .notEmpty().withMessage('Debes completar el campo Confirmar Contraseña').bail()
+        .custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error('Las contraseñas no coinciden');
+            }
+            return true;
+        }),
+    body('image').custom((value, { req }) => {
+        let file = req.file;
+        if (!file) {
+            throw new Error('Debes subir una imagen');
+        }
+        return true;
+    })
+];
+
 router.get('/login', usersController.login);
 
 router.get('/register', usersController.registro);
 
-router.post('/register', upload.single('image'), usersController.newUser);
+router.post('/register', upload.single('image'), validations, usersController.newUser);
 
 module.exports = router;
